c814ee69236ee38e76d88779a0ed57267db8c89d
rohansingh97 <rohansingh@0597>
2020-01-24 12:15:12 +0530

    fuck u

---
+++ src/main/java/com/rakuten/client/CousreClient.java
@@ -0,0 +1,13 @@
+package com.rakuten.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CousreClient {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CousreClient.class, args);
+	}
+
+}
